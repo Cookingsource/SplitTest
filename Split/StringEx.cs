3f22917099afb4ad24406f756f7bb695ea1165f1
Rafael Muñoz <r.munoz.software@gmail.com>
2019-08-26 14:19:40 +0200

    Created project and StringEx file

---
+++ Split/StringEx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Split
+{
+    public class StringEx
+    {
+        public static string[] Split(string source, string delimiter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
