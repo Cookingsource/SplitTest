66d7f42cc71faafb949b62f1fa07a522f378eb33
Rafael Muñoz <r.munoz.software@gmail.com>
2019-08-26 15:17:08 +0200

    Added some basic input guards and degenerate cases

--- Split/StringEx.cs
+++ Split/StringEx.cs
@@ -5,6 +5,12 @@ namespace Split
     {
         public static string[] Split(string source, string delimiter)
         {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (delimiter == null || delimiter == string.Empty )
+                return new string[] { source };
+
             throw new NotImplementedException();
         }
     }
---
+++ Tests/SplitTests.cs
@@ -0,0 +1,39 @@
+using System;
+using NSubstitute;
+using NUnit.Framework;
+using Split;
+
+namespace Tests
+{
+    public class SplitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Split_NullSource_Fails()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                StringEx.Split(null, Arg.Any<string>());
+            });
+        }
+
+        [Test]
+        public void Split_NullOrEmptySeparator_ReturnsSource()
+        {
+            string source = "SomeString";
+            AssertSplit(source, null, source);
+            AssertSplit(source, string.Empty, source);
+        }
+
+        private void AssertSplit(string source, string delimiter, params string[] expectedResult)
+        {
+            CollectionAssert.AreEqual(
+                expectedResult,
+                StringEx.Split(source, delimiter));
+        }
+    }
+}
